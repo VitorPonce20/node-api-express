cd791fca4973e7affc90612ad74805c375b1c65b
Vitor Ponce <vitor.ponce20@gmail.com>
2025-01-07 23:26:18 -0300

    [1] mongoose config

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Mongoose library instance
+const mongoose = require('mongoose');
+//URL of MongoDB Database
+const mongoDBURL = 'mongodb://127.0.0.1:27017/Test';
+
+//connect to Database
+mongoose.connect(mongoDBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => { console.log('Connection Successfull') })
+    .catch((err) => { console.log('Received an Error') })
+
 app.get('/', (req,res) => {res.send("Hello World")})
 
 app.listen(port, () => {console.log(`App listening on port ${port}`)})
     
+

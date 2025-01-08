6be8156fde35b41d6fc2e2f3cfea94fc06d1b2cd
Vitor Ponce <vitor.ponce20@gmail.com>
2025-01-08 00:33:02 -0300

    [2] added new schema

--- app.js
+++ app.js
@@ -15,8 +15,40 @@ mongoose.connect(mongoDBURL, {
     .then(() => { console.log('Connection Successfull') })
     .catch((err) => { console.log('Received an Error') })
 
-app.get('/', (req,res) => {res.send("Hello World")})
+
+
 
 app.listen(port, () => {console.log(`App listening on port ${port}`)})
+
+const {Schema, model} = mongoose;
+
+const schema = new Schema({
+    name:String,
+    type:String,
+    shiny:Boolean,
+    tags:[String]
+})
     
+const Poke = model('Poke', schema);
+// export default Poke;
+
+const Charmander = new Poke({
+    name:'Charmander',
+    type:'Fire',
+    shiny:true, 
+    tags:['fire', 'shiny']
+})
+
+const Bulbasaur = new Poke({
+    name:'Bulbasaur',
+    type:'Water',
+    shiny:false, 
+    tags:['water', 'shiny']
+})
+
+Charmander.save();
+
+app.get('/', (req,res) => { res.send("Hello World");
+    Bulbasaur.save();    
+})
 
